6cc40a2e7721ca59764ba5febdb6f0c2c175968f
Boyko Alexey <elorian@yandex.ru>
2020-11-15 19:06:48 +0300

    add  field in admin

--- pages/admin.py
+++ pages/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Arm
 
-admin.site.register(Arm)
+
+class PostArm(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("name_arm", "id_arm", "dt_version_arm")
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("name_arm",)
+    # добавляем возможность фильтрации по дате
+    list_filter = ("name_arm",)
+
+admin.site.register(Arm, PostArm)
