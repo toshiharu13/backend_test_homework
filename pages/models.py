1f99d3d15e20e9872d2ef12ae5615fda87f5687b
Boyko Alexey <elorian@yandex.ru>
2020-11-17 10:00:21 +0300

    add view objects in adminkka

--- pages/models.py
+++ pages/models.py
@@ -7,3 +7,6 @@ class Arm(models.Model):
     dt_version_arm = models.CharField(max_length=30, blank=True)
     os_version_arm = models.CharField(max_length=30, blank=True)
 
+    def __str__(self):
+        return self.name_arm
+
